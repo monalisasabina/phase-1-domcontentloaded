20b4410b6ef279605c3e060e491b939d717523af
Monalisa Onyango <+>
2024-07-08 17:41:42 +0300

    DOMCONTECTLOADED

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
-// Your code goes here
+document.addEventListener( "DOMContentLoaded", function () {
+  changeText()
+} );
+
+function changeText() {
+  document.getElementById( "text" )
+    .textContent= "This is really cool!";
+}
+
+
+
+
